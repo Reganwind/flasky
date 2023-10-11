70510591787d289eea54c9a21851f5e30153d654
zhangsc <zhangsc@asiainfo.com>
2023-10-11 10:37:26 +0800

    Ready to deploy

--- 2_1/app2_3.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
--- k.py
+++
@@ -1,4 +0,0 @@
-def sayhello(to=None):
-    if to:
-        return f'hello, {to}!'
-    return 'Hello!'
